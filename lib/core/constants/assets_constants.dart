ca29086b350e7cbe9b094f9f229477337f834b8c
Flavio Vieira <flavioemerson1@outlook.com>
2023-03-04 20:47:48 -0300

    feito esboço da home page

--- lib/core/constants/assets_constants.dart
+++ lib/core/constants/assets_constants.dart
@@ -1,4 +1,5 @@
 class AssetsConstants {
   static const logoWhite = 'assets/images/logo_white.png';
+  static const homeBackground = 'assets/images/Flutter SO-logos.jpeg';
   static const loadingLottie = 'assets/lotties/loading.json';
 }
--- lib/features/home_so/presenter/home_so_page.dart
+++ lib/features/home_so/presenter/home_so_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_so/core/constants/assets_constants.dart';
 import 'package:flutter_so/features/home_so/presenter/widgets/home_so_bottom_navbar.dart';
+import 'package:sizer/sizer.dart';
 
 class HomeSoPage extends StatelessWidget {
   const HomeSoPage({Key? key}) : super(key: key);
@@ -7,9 +9,28 @@ class HomeSoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
       bottomNavigationBar: const HomeSoBottomNavbar(),
       body: Stack(
-        children: [],
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage(
+                  AssetsConstants.homeBackground,
+                ),
+              ),
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.folder_copy,
+              size: 18.sp,
+            ),
+          ),
+        ],
       ),
     );
   }
